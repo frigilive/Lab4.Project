7ab2dc2bed3e706ffb1d53fd9f7a7e68598648c3
Vino M <vinoshun.manoratnam@ryerson.ca>
2013-11-24 20:27:03 -0500

    patients source

--- Lab4/src/patients.cpp
+++ Lab4/src/patients.cpp
@@ -4,10 +4,11 @@
  *  Created on: Nov 24, 2013
  *      Author: jfrigill
  */
+#include "patients.hpp"
 
 patient :: patient()
 {
-	cout << "Please enter the patient's first name"
+	cout << "Please enter the patient's first name";
 }
 
 
