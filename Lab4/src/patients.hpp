05e6a1a3cbf9cb93c3f78c50f9c3349c32fb8d26
Vino M <vinoshun.manoratnam@ryerson.ca>
2013-11-24 20:21:35 -0500

    patient header

--- Lab4/src/patients.hpp
+++ Lab4/src/patients.hpp
@@ -1,14 +1,28 @@
 /*
- * patients.hpp
- *
- *  Created on: Nov 24, 2013
- *      Author: jfrigill
- */
+* patient.hpp
+*
+* Created on: Nov 24, 2013
+* Author: vmanorat
+*/
 
-#ifndef PATIENTS_HPP_
-#define PATIENTS_HPP_
+#ifndef PATIENT_HPP_
+#define PATIENT_HPP_
+#include <string>
+#include <iostream>
+using namespace std;
+
+class patient{
+        string fName;
+        string lName;
+        int age;
+        string SIN;
+        string *annotation;
+public:
+        patient;
+        void display;
+        void results;
+        ~patient;
+};
 
 
-
-
-#endif /* PATIENTS_HPP_ */
+#endif /* PATIENT_HPP_ */
